2a05e458d4327bbfdd292e3339c2dd8fd9ffd198
amith-george <amithgeorge130@gmail.com>
2024-03-30 11:40:49 +0530

    Final

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let pCount = 0
+let cCount = 0;
+let Pchoice;
+let Rchoice;
+
 function getComputerChoice() {
     let rnum = Math.floor(Math.random()*3);
     if (rnum == 0) {
@@ -36,10 +41,36 @@ function playGame(pchoice,cchoice){
     }
 }
 
-let pCount = 0;
-let cCount = 0;
-console.log(typeof(pCount));
-let Pchoice = prompt("Enter your choice: ");
-Pchoice = Pchoice.toLowerCase();
-let Rchoice = getComputerChoice();
-playGame(Pchoice,Rchoice);
+
+function fiveround(){
+    for(let i=0; i<5; i++)
+    {
+        Pchoice = prompt("Enter your choice (Rock/Paper/Scissors): ");
+        Pchoice = Pchoice.toLowerCase();
+        Rchoice = getComputerChoice();
+        if (Pchoice != "rock" && Pchoice != "paper" && Pchoice != "scissors")
+        {
+            alert("Enter a proper choice idiot!");
+            return;
+        }
+        else{
+        playGame(Pchoice,Rchoice);
+        }
+    }
+
+    if (pCount == cCount){
+        console.log("You tied with the AI with the score "+pCount+" and "+cCount+"!");
+    }
+    else if (pCount > cCount)
+    {
+        console.log("You won against the AI with the score "+pCount+" and "+cCount+"!");
+    }
+    else {
+        console.log("You lost against the AI with the score "+pCount+" and "+cCount+"!");
+    }
+}
+
+fiveround();
+
+
+
